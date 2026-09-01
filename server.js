04c5e8bcb01a07fdf1db3720f585f83109e0fdbd
Tang <jeancarloserrano0410@gmail.com>
2026-09-02 04:49:59 +0800

    server.js changes

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const PORT = process.env.PORT || 3003;
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json()); 
 
+app.get('/', (req, res) => {
+    res.redirect('/login.html');
+});
+
 // ==========================================
 // --- TEMPORARY DATABASES ---
 // ==========================================
